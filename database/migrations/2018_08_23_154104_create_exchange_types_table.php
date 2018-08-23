b465fb6ee9b44db9740c914b34c8622a46ec36d3
bek <b.isakhmet@prodengi.kz>
2018-08-23 15:56:04 +0600

    Типы курсов обмена
    - модели
    - миграций
    - сиды

---
+++ database/migrations/2018_08_23_154104_create_exchange_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExchangeTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange_types', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title');
+            $table->string('name');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange_types');
+    }
+}
---
+++ database/seeds/ExchangeTypeSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ExchangeTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(\App\Models\ExchangeType::all()->count() === 0){
+
+            $date = date('Y-m-d H:i:s');
+
+            $data = [
+                [
+                    'title' => 'Наличный',
+                    'name'=> 'cash',
+                    'created_at' => $date
+                ],
+                [
+                    'title' => 'По платежным карточкам',
+                    'name'=> 'cards',
+                    'created_at' => $date
+                ],
+                [
+                    'title' => 'Безналичный',
+                    'name'=> 'transfer',
+                    'created_at' => $date
+                ],
+
+            ];
+            /** @noinspection PhpUndefinedMethodInspection */
+            \App\Models\ExchangeType::insert($data);
+            $this->command->info('Таблица типов обмена заполнена');
+        }else{
+            $this->command->warn('Таблица типов обмена уже заполнена');
+        }
+    }
+}
