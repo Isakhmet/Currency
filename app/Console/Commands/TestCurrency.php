70c0b2489cec360c2ec0023db8f7cc3c9c87afe9
bek <b.isakhmet@prodengi.kz>
2018-09-11 18:30:37 +0600

    Для запуска парсеров Формат кода и документация

---
+++ app/Console/Commands/TestCurrency.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+
+use App\Classes\Process\GetCurrencyRateProcess;
+use App\Classes\Process\ProcessContext;
+use App\Models\CompanyLink;
+use Illuminate\Console\Command;
+
+class TestCurrency extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:cur';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $model = CompanyLink::get(['company_id', 'exchange_type_id'])->toArray();
+
+        foreach ($model as $data) {
+            $gcrp = new GetCurrencyRateProcess(new ProcessContext(), $data['company_id'], $data['exchange_type_id']);
+            $gcrp->run();
+        }
+    }
+}
